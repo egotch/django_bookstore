c2c9a32d1d98b9597601efa02bbeaaf215ab7c16
Eric Gotkowski <egotkowski@gmail.com>
2024-06-12 22:35:26 -0500

    feature/user-accounts - 2024-06-12 22:35
     - removed pages app (no longer needed)
     - started custom user forms
     - created custom user model placeholder

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 #TODO: create new CustomUser model that extends AbtractUser
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
--- django_project/settings.py
+++ django_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Local
-    "pages.apps.PagesConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +127,4 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "accounts.CustomUser"
