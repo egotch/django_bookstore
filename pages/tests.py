f5a2c57efa8536e16a8ee904096020c42673ee36
Eric Gotkowski <egotkowski@gmail.com>
2024-06-17 22:35:30 -0500

    homepage
    - adding test suite for home page

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
