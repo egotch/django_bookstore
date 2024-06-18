c556dbb69fc7c353bba14fb247a0862e308bab29
Eric Gotkowski <egotkowski@gmail.com>
2024-06-17 22:49:55 -0500

    home page testing
    - added postive and negative html testing
     - added simplifying abstraction to tests

--- pages/tests.py
+++ pages/tests.py
@@ -5,14 +5,22 @@ from django.urls import reverse
 
 class HomepageTests(SimpleTestCase):
 
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
     def test_url_exists_at_correct_location(self):
-        response = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_url(self):
-        response = self.client.get(reverse("home"))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
 
     def test_homepage_template(self):
-        response = self.client.get("/")
-        self.assertTemplateUsed(response, "home.html")
+        self.assertTemplateUsed(self.response, "home.html")
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(self.response, "home page")
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(self.response, "Hi there!  I should not be on the page...")
+        pass
