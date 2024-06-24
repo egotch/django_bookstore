4cae77f78a9a4814ce99b986153ad176e94b5d1c
Eric Gotkowski <egotkowski@gmail.com>
2024-06-24 00:17:40 -0500

    feature/static-content
    - adding about page test scripts

--- pages/tests.py
+++ pages/tests.py
@@ -24,3 +24,25 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_does_not_contain_incorrect_html(self):
         self.assertNotContains(self.response, "Hi there!  I should not be on the page...")
         pass
+
+
+class AboutPageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse("about")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_url(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_aboutpage_template(self):
+        self.assertTemplateUsed(self.response, "about.html")
+
+    def test_aboutpage_contains_correct_html(self):
+        self.assertContains(self.response, "about")
+
+    def test_aboutpage_does_not_contain_incorrect_html(self):
+        self. assertNotContains(self.response, "Hello, This does not feel like th right txt ...")
