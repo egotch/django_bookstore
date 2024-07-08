e97d0a712e3562c0c447eeaf14039715502d9e2f
Eric Gotkowski <egotkowski@gmail.com>
2024-07-07 23:05:17 -0500

    book list page
    - created simple book listing page
     - registered views to admin for ez book adding

--- books/admin.py
+++ books/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Book
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ["title", "author", "price"]
+
+
+admin.site.register(Book, BookAdmin)
