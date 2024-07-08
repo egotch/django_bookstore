57de7462a772c5d506cbbe49355fd97b56aa43cd
Eric Gotkowski <egotkowski@gmail.com>
2024-07-07 23:32:02 -0500

    individual book views
    - adding book detail view
     - hooking up book pk's to detail views
     - adding book list view/page

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Book
 
 
@@ -6,3 +6,9 @@ class BookListView(ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = "book"
+    template_name = "books/book_detail.html"
