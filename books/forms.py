d29a8b227c2fbcaaebdb435fbecc4304da4e3cca
Eric Gotkowski <egotkowski@gmail.com>
2024-07-24 21:03:56 -0500

    adding book creation form

---
+++ books/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Book
+
+
+class BookCreationForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = {
+            "title",
+            "author",
+            "price",
+            "summary",
+        }
